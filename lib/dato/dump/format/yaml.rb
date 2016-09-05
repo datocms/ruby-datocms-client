2bd52ba41302d4db3fc90abca5e32e56f0300274
Stefano Verna <s.verna@cantierecreativo.net>
2016-09-05 10:30:55 +0200

    stringify_keys for Array

--- lib/dato/dump/format/yaml.rb
+++ lib/dato/dump/format/yaml.rb
@@ -2,6 +2,20 @@
 require 'active_support/core_ext/hash/keys'
 require 'yaml'
 
+class Array
+  def deep_stringify_keys
+    inject([]) { |accum, value|
+      if (value.is_a?(Hash) or value.is_a?(Array))
+        new_val = value.deep_stringify_keys
+        accum.push new_val
+      else
+        accum.push value
+      end
+      accum
+    }
+  end
+end
+
 module Dato
   module Dump
     module Format
---
+++ spec/dato/dump/format/yaml_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+module Dato
+  module Dump
+    module Format
+      RSpec.describe Yaml do
+        describe '.dump' do
+          it 'dumps into Yaml stringifying symbols' do
+            expect(Yaml.dump([{ foo: "bar" }])).to eq "- foo: bar"
+          end
+        end
+      end
+    end
+  end
+end
+
