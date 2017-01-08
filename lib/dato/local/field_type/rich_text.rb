08209da16233b1e4096327033d67dbf5ca6d9630
Stefano Verna <s.verna@cantierecreativo.net>
2017-01-08 16:06:55 +0100

    Add RichText field type

---
+++ lib/dato/local/field_type/rich_text.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+module Dato
+  module Local
+    module FieldType
+      class RichText < Array
+        def self.parse(ids, repo)
+          items = if ids
+                    ids.map { |id| repo.find(id) }
+                  else
+                    []
+                  end
+          new(items)
+        end
+
+        def to_hash
+          map(&:to_hash)
+        end
+      end
+    end
+  end
+end
